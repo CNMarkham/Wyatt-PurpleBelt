44b4fd3028e772c4f5b78dd38848dccc8cbb49c3
alison <markhamonca@codeninjas.com>
2023-07-05 18:00:19 -0400

    dxcfvghjkl;okijuhygtfrtgbyhftj
    step 103

---
+++ Old yellow ball ghost scary maze thing/Assets/Scripts/Ghost.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
