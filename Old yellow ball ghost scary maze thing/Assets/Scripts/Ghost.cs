1be2e0e5d694ecfcdfe67793efe28ec311e42c0c
alison <markhamonca@codeninjas.com>
2023-07-12 18:00:50 -0400

    Step 123!
    Scatter script brokeen!

--- Old yellow ball ghost scary maze thing/Assets/Scripts/Ghost.cs
+++ Old yellow ball ghost scary maze thing/Assets/Scripts/Ghost.cs
@@ -21,7 +21,12 @@ public class Ghost : Movement
 
     private void Awake()
     {
-        
+        body.SetActive(true);
+        eyes.SetActive(true);
+        blue.SetActive(false);
+        white.SetActive(false);
+        frightened = false;
+        Invoke("LeaveHome", homeDuration);
     }
 
     protected override void ChildUpdate()
@@ -31,7 +36,10 @@ public class Ghost : Movement
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (atHome && collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            SetDirection(-direction);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -42,12 +50,33 @@ public class Ghost : Movement
         {
             int index = Random.Range(0, node.availableDirections.Count);
 
+
+   
+            if (node.availableDirections[index] == -direction)
+            {
+                index += 1;
+
+                if(index == node.availableDirections.Count)
+                {
+                    index = 0;
+                }
+
+            }
+
             SetDirection(node.availableDirections[index]);
         }
     }
 
     private void LeaveHome()
     {
+        transform.position = new Vector3(5.39f, 3.54f, -1.023971f);
+        direction = new Vector2(-1, 0);
+        atHome = false;
+        frightened = false;
+        body.SetActive(true);
+        eyes.SetActive(true);
+        blue.SetActive(false);
+        white.SetActive(false);
 
     }
 
