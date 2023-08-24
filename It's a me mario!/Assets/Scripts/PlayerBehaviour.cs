0bb1e996d219d99c4def4fc4f9b79c674c03d1b9
CNMarkham <96213580+ilovetapwaterr@users.noreply.github.com>
2023-08-24 19:01:43 -0400

    step 110
    lol cool

---
+++ It's a me mario!/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,107 @@
+using System.Collections;                                 //     | \ |
+                                                          //     |   \
+                                                          //     |   |
+                                                          //     |  /
+                                                          //      /  |
+                                                          //     |   |
+using System.Collections.Generic;                         //    function
+using UnityEngine;                                        //   couroutine
+                                                          //     |   |
+                                                          //     |  /|
+                                                          //      /  |
+                                                          //     | \ |
+                                                          //     |   \
+                                                          //     |   |
+                                                          //     |  /
+                                                          //      /  |
+                                                          //     |   |                
+public class PlayerBehaviour : MonoBehaviour
+{
+    public SpriteRenderer smallRenderer;
+    public SpriteRenderer bigRenderer;
+    private Animator smallAnimator;
+    public bool big;
+
+    public void Hit()
+    {
+        if(big)
+        {
+            Shrink();
+        }else
+        {
+            Death();
+        }
+    }
+
+    private void Shrink()
+    {
+        smallRenderer.enabled = true;
+            bigRenderer.enabled = false;
+
+        GetComponent<CapsuleCollider2D>().size = Vector2.one;
+        GetComponent<CapsuleCollider2D>().offset = Vector2.zero;
+
+        big = false;
+        StartCoroutine("ChangeSize");
+    }
+
+     public void Grow()
+    {
+        if(big)
+        {
+            return;
+        }
+
+        smallRenderer.enabled = false;
+        bigRenderer.enabled = true;
+
+        GetComponent<CapsuleCollider2D>().size = new Vector2(1f, 2f);
+        GetComponent<CapsuleCollider2D>().offset = new Vector2(0, 0.5f);
+
+        big = true;
+        StartCoroutine("ChangeSize");
+    }
+
+    private void Death()
+    {
+        smallAnimator.SetTrigger("death");
+
+        GetComponent<CapsuleCollider2D>().enabled = false;
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * 10;
+        GetComponent<PlayerMovement>().enabled = false;
+        Destroy(gameObject, 0.5f);
+    }
+
+    private IEnumerator ChangeSize()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector3 velocity = rb.velocity;
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        rb.isKinematic = true; //kinematic means it does not get affected by physics
+        rb.velocity = Vector3.zero;
+        for (int i = 0; i < 8; i++)
+        {
+            bigRenderer.enabled ^= true; // ^= operator means to the power of
+            smallRenderer.enabled ^= true;
+            yield return new WaitForSeconds(0.25f);
+        }
+        rb.isKinematic = false;
+        rb.velocity = Vector3.zero;
+        GetComponent<PlayerMovement>().enabled = true;
+        GetComponent<CapsuleCollider2D>().enabled = true;
+
+    }
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
